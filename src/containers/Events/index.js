aa7c352104a6da95f88cef6641b61da9f8b711cc
JP Brunache <kosmik7@users.noreply.github.com>
2023-04-18 14:39:22 +0200

    Events: correction du filtre/affichage des events

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import EventCard from "../../components/EventCard";
 import Select from "../../components/Select";
 import { useData } from "../../contexts/DataContext";
@@ -13,25 +13,31 @@ const EventList = () => {
   const { data, error } = useData();
   const [type, setType] = useState();
   const [currentPage, setCurrentPage] = useState(1);
-  const filteredEvents = (
-    (!type
-      ? data?.events
-      : data?.events) || []
-  ).filter((event, index) => {
-    if (
-      (currentPage - 1) * PER_PAGE <= index &&
-      PER_PAGE * currentPage > index
-    ) {
-      return true;
-    }
-    return false;
-  });
+  const [filteredEvents, setFilteredEvents] = useState([]);
+  const typeList = new Set(data?.events.map((event) => event.type));
+
+  useEffect(() => {
+    const filterType =
+      (!type
+        ? data?.events
+        : data?.events.filter((event) => event.type === type)) || [];
+    const filtered = filterType.filter((_, index) => {
+      if (
+        (currentPage - 1) * PER_PAGE <= index &&
+        PER_PAGE * currentPage > index
+      ) {
+        return true;
+      }
+      return false;
+    });
+    setFilteredEvents(filtered);
+  }, [data, type, currentPage]);
+
   const changeType = (evtType) => {
     setCurrentPage(1);
     setType(evtType);
   };
   const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
-  const typeList = new Set(data?.events.map((event) => event.type));
   return (
     <>
       {error && <div>An error occured</div>}
