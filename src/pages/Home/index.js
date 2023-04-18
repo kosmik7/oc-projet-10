a3cae29de69116aef778f2feb61578f378a1f66f
JP Brunache <kosmik7@users.noreply.github.com>
2023-04-18 15:48:18 +0200

    Typo

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -58,7 +58,7 @@ const Page = () => {
       </section>
       <section className="PeoplesContainer">
         <h2 className="Title">Notre équipe</h2>
-        <p>Une équipe d’experts dédiés à l’ogranisation de vos événements</p>
+        <p>Une équipe d’experts dédiés à l’organisation de vos événements</p>
         <div className="ListContainer">
           <PeopleCard
             imageSrc="/images/stephanie-liverani-Zz5LQe-VSMY-unsplash.png"
